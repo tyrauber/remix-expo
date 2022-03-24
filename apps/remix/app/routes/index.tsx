e0e3917fc5dc7d4161b5373d045f673abc6f032d
Ty Rauber <tyrauber@mac.com>
2022-03-24 10:08:25 -0400

    feat: add tailwind through twrnc on packages/ui

--- apps/remix/app/routes/index.tsx
+++ apps/remix/app/routes/index.tsx
@@ -1,19 +1,15 @@
-import { View, Text, StyleSheet } from 'react-native-web'
+import { View } from 'react-native-web'
 import DefaultText from '~components/index'
 
 export const Page = ()=> {
   return (
-    <View style={styles.container}>
+    <View style={{
+    alignItems: 'center',
+    justifyContent: 'center'
+  }}>
       <DefaultText/>
     </View>
   );
 }
 
 export default Page
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-});
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,6 +1,11 @@
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
+import tw from 'twrnc';
 
 export const App = ()=> {
-  return (<Text>Open up App.tsx to start working on your app!</Text>)
+  return (
+    <View style={tw`p-4 android:pt-2 bg-white dark:bg-black`}>
+      <Text style={tw`text-2xl text-black dark:text-white`}>Open up packages/ui/index.tsx to start working on your app!</Text>
+    </View>
+  )
 }
 export default App
